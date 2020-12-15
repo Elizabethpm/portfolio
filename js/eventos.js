9a95315b5ce30e86f2dd309b60a1ec1e44dfade3
Elizabethpm <76049957+Elizabethpm@users.noreply.github.com>
2020-12-15 10:32:50 -0400

    Update eventos.js

--- js/eventos.js
+++ js/eventos.js
@@ -119,4 +119,17 @@ $(document).ready(function () {
     $("#seleccionTema5").hide();
     $("#elegirTema").show();
   });
+
+  //Scroll para subir al inicio de la web
+  $(".btn-subir").click(function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      800
+    );
+
+    return false;
+  });
 });
